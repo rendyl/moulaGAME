76546b856ec1dca0a21fe5b34f476b8213271720
p1516923 <rendy.latbi@etu.univ-lyon1.fr>
2019-10-28 20:36:49 +0100

    doors

---
+++ ZombiZ/Assets/Scripts/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : ShopManager
+{
+    public override void shopping()
+    {
+        textSHOP.SetText("");
+        Destroy(transform.parent.gameObject);
+    }
+}
